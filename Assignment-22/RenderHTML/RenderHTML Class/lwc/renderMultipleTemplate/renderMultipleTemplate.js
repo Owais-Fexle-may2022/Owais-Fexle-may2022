5b6233c8ada2ce7ef8e22ef39b02a058237660a2
Owais-Fexle-may2022 <mohd.owais@fexle.com>
2022-09-07 14:19:57 +0530

    Update(LWC Assignment-22)

--- Assignment-22/RenderHTML/RenderHTML Class/lwc/renderMultipleTemplate/renderMultipleTemplate.js
+++ Assignment-22/RenderHTML/RenderHTML Class/lwc/renderMultipleTemplate/renderMultipleTemplate.js
@@ -1,76 +1,52 @@
-import { LightningElement,track,wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import getTextMethod1 from '@salesforce/apex/Controller.getTextMethod1';
 import getTextMethod2 from '@salesforce/apex/Controller.getTextMethod2';
 import getTextMethod3 from '@salesforce/apex/Controller.getTextMethod3';
 
+
 export default class RenderMultipleTemplate extends LightningElement {
-    @track message1="";
-    @track message2="";
-    @track message3="";
+    message1;
+    message2;
+    message3;
     handleClickMe1() {
-        this.invokeApexMethods1();
+        getTextMethod1()
+            .then(result1 => {
+                this.message1 = result1;
+            })
+            .catch(error => {
+                this.error=error;
+            });
     }
 
     handleClickMe2() {
-        this.invokeApexMethods2();
+        getTextMethod1()
+            .then(result1 => {
+                this.message1 = result1;
+            })
+        getTextMethod2()
+            .then(result2 => {
+                this.message2 =result2;
+            })
+            .catch(error => {
+                this.error=error;
+            });
     }
 
     handleClickMe3() {
-        this.invokeApexMethods3();
-    }
-
-    invokeApexMethods3() {
         getTextMethod1()
-        .then((result1) => {
-            this.message1 = ('Method1 result: ' + result1);
- 
-            getTextMethod2()
-            .then((result2) => {
-                this.message2 = ('Method2 result: ' + result1 + ' ' + result2);
-
-                getTextMethod3()
-                .then((result3) => {
-                    this.message3 = ('Method3 result: ' + result1 + ' ' + result2 + ' ' + result3);
-                })
-                .catch((error => {
-                    console.log(error);
-                }));
+            .then(result1 => {
+                this.message1 = result1;
             })
-            .catch((error => {
-                console.log(error);
-            }));
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
-
-    invokeApexMethods2() {
-        getTextMethod1()
-        .then((result1) => {
-            this.message1 = ('Method1 result: ' + result1);
- 
-            getTextMethod2()
-            .then((result2) => {
-                this.message2 = ('Method2 result: ' + result1 + ' ' + result2);
+        getTextMethod2()
+            .then(result2 => {
+                this.message2 =result2;
             })
-            .catch((error => {
-                console.log(error);
-            }));
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
-
-    invokeApexMethods1() {
-        getTextMethod1()
-        .then((result1) => {
-            this.message1 = ('Method1 result: ' + result1);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
-        
+        getTextMethod3()
+            .then(result3 => {
+                this.message3 = result3;
+            })
+            .catch(error => {
+                this.error=error;
+            });
+    }        
 }
