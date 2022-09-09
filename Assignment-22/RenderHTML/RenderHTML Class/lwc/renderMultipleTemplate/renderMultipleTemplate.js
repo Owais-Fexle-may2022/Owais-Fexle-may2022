9deea44b90d0d3f1a255d257d240f610b4fb63b8
Owais-Fexle-may2022 <mohd.owais@fexle.com>
2022-09-09 14:03:50 +0530

    update_Assignment(All)

--- Assignment-22/RenderHTML/RenderHTML Class/lwc/renderMultipleTemplate/renderMultipleTemplate.js
+++ Assignment-22/RenderHTML/RenderHTML Class/lwc/renderMultipleTemplate/renderMultipleTemplate.js
@@ -1,14 +1,24 @@
+  /*
+    ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+    * @Description      This Js file is used as to import string type of data from apex class and show return value to HTML template.
+    * @author           Mohd Owais  <mohd.owais@fexle.com>
+    * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+    */  
 import { LightningElement } from 'lwc';
+// Importing methods of apex class.
 import getTextMethod1 from '@salesforce/apex/Controller.getTextMethod1';
 import getTextMethod2 from '@salesforce/apex/Controller.getTextMethod2';
 import getTextMethod3 from '@salesforce/apex/Controller.getTextMethod3';
 
 
 export default class RenderMultipleTemplate extends LightningElement {
+    // initilizing variables
     message1;
     message2;
     message3;
+    // When Hit click me 1  button this methods start executing
     handleClickMe1() {
+        // this promise gave two value if result found then gave value else gave error
         getTextMethod1()
             .then(result1 => {
                 this.message1 = result1;
@@ -17,7 +27,7 @@ export default class RenderMultipleTemplate extends LightningElement {
                 this.error=error;
             });
     }
-
+    // When Hit click me 2  button this methods start executing
     handleClickMe2() {
         getTextMethod1()
             .then(result1 => {
@@ -31,7 +41,7 @@ export default class RenderMultipleTemplate extends LightningElement {
                 this.error=error;
             });
     }
-
+    // When Hit click me 3  button this methods start executing
     handleClickMe3() {
         getTextMethod1()
             .then(result1 => {
